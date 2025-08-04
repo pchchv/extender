56ce38a3533a8fe23814ab6980bca2267456abe9
pchchv <ipchchv@gmail.com>
2025-08-04 10:44:20 +0300

    Add tests for Instant type

---
+++ time/instant_test.go
@@ -0,0 +1,19 @@
+package timeext
+
+import "testing"
+
+func TestInstant(t *testing.T) {
+	i := NewInstant()
+	if i.Elapsed() < 0 {
+		t.Fatalf("elapsed time should be always be monotonically increasing")
+	}
+
+	i2 := NewInstant()
+	if i2.Since(i) < 0 {
+		t.Fatalf("time since instant should always be after")
+	}
+
+	if i.Since(i2) != 0 {
+		t.Fatalf("time since instant should be zero")
+	}
+}
