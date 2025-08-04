b93df11e837e40dd6341f266f5acf1e16d959ff3
pchchv <ipchchv@gmail.com>
2025-08-04 10:40:57 +0300

    Add benchmarks for NanoTime

--- time/nanotime_test.go
+++ time/nanotime_test.go
@@ -13,3 +13,15 @@ func TestNanoTime(t *testing.T) {
 		t.Fatalf("nanotime failed to monotonically increase, t1: %d t2: %d", t1, t2)
 	}
 }
+
+func BenchmarkNanoTime(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NanoTime()
+	}
+}
+
+func BenchmarkNanoTimeUsingUnixNano(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = time.Now().UnixNano()
+	}
+}
