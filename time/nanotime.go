b2fb9b8d9bc1f46427af3f723c813ffd375617bf
pchchv <ipchchv@gmail.com>
2025-08-04 10:39:31 +0300

    Implement NanoTime function

---
+++ time/nanotime.go
@@ -0,0 +1,10 @@
+package timeext
+
+import "time"
+
+var base = time.Now()
+
+// NanoTime returns a monotonically increasing time in nanoseconds.
+func NanoTime() int64 {
+	return int64(time.Since(base))
+}
