821c5f74f4055549d7316ce6a799954d72629203
pchchv <ipchchv@gmail.com>
2025-08-04 10:32:18 +0300

    Implement a StringToBytes func

--- unsafe/conversions.go
+++ unsafe/conversions.go
@@ -7,3 +7,10 @@ import "unsafe"
 func BytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
+
+// StringToBytes converts an existing string into an []byte without allocating.
+// The string passed to this functions is not to be used again after this call as it's unsafe.
+func StringToBytes(s string) []byte {
+	stringPtr := unsafe.StringData(s)
+	return unsafe.Slice((*byte)(stringPtr), len(s))
+}
