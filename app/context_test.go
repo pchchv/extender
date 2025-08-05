2ff9e5916a2100e27794e55d4e95399303622116
pchchv <ipchchv@gmail.com>
2025-08-05 12:14:33 +0300

    Add tests for force exit context

---
+++ app/context_test.go
@@ -0,0 +1,84 @@
+package appext
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"testing"
+	"time"
+
+	. "github.com/pchchv/go-assert"
+)
+
+func TestForceExitWithNoTimeout(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	exitFn := func(code int) {
+		defer wg.Done()
+		Equal(t, 1, code)
+	}
+
+	c := Context().Timeout(0).ExitFn(exitFn)
+
+	// copy of Build for testing
+	var sig = make(chan os.Signal, 1)
+	signal.Notify(sig, c.signals...)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go listen(sig, cancel, c.exitFn, c.timeout, c.forceExit)
+
+	sig <- os.Interrupt
+	sig <- os.Interrupt
+	wg.Wait()
+	Equal(t, context.Canceled, ctx.Err())
+}
+
+func TestForceExitWithTimeout(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	exitFn := func(code int) {
+		defer wg.Done()
+		Equal(t, 1, code)
+	}
+
+	c := Context().Timeout(time.Hour).ExitFn(exitFn)
+
+	// copy of Build for testing
+	var sig = make(chan os.Signal, 1)
+	signal.Notify(sig, c.signals...)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go listen(sig, cancel, c.exitFn, c.timeout, c.forceExit)
+
+	sig <- os.Interrupt
+	sig <- os.Interrupt
+	wg.Wait()
+	Equal(t, context.Canceled, ctx.Err())
+}
+
+func TestTimeoutWithNoForceExit(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	exitFn := func(code int) {
+		defer wg.Done()
+		Equal(t, 1, code)
+	}
+
+	c := Context().Timeout(time.Millisecond * 200).ForceExit(false).ExitFn(exitFn)
+
+	// copy of Build for testing
+	var sig = make(chan os.Signal, 1)
+	signal.Notify(sig, c.signals...)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go listen(sig, cancel, c.exitFn, c.timeout, c.forceExit)
+
+	// only sending one, timeout must be reached for test to finish
+	sig <- os.Interrupt
+	wg.Wait()
+	Equal(t, context.Canceled, ctx.Err())
+}
