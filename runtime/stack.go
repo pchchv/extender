725f1f0e963c23745b01f791bf68230f3fb79fd6
pchchv <ipchchv@gmail.com>
2025-08-05 11:53:43 +0300

    Implement Function method for Frame type

--- runtime/stack.go
+++ runtime/stack.go
@@ -1,9 +1,19 @@
 package runtimeext
 
-import "runtime"
+import (
+	"runtime"
+	"strings"
+)
 
 // Frame wraps a runtime.Frame to provide some helper functions while
 // still allowing access to the original runtime.Frame.
 type Frame struct {
 	runtime.Frame
+}
+
+// Function is the runtime.Frame.Function stripped down to just the function name.
+func (f Frame) Function() string {
+	name := f.Frame.Function
+	i := strings.LastIndexByte(name, '.')
+	return name[i+1:]
 }
