1bf59752b39af0088dda1dad2fa31d5fd1d16ced
pchchv <ipchchv@gmail.com>
2025-08-06 09:28:32 +0300

    Implement IsTemporary and IsTemporaryConnection functions

--- errors/retryable.go
+++ errors/retryable.go
@@ -1,6 +1,9 @@
 package errorsext
 
-import "errors"
+import (
+	"errors"
+	"syscall"
+)
 
 // IsRetryable returns true if the provided error is considered retryable by
 // testing if it complies with an interface implementing `Retryable() bool` or
@@ -17,4 +20,54 @@ func IsRetryable(err error) bool {
 		Retryable() bool
 	}
 	return errors.As(err, &t2) && t2.Retryable()
+}
+
+// IsTemporary returns true if the provided error is
+// considered retryable temporary error by testing if
+// it complies with an interface implementing `Temporary() bool` and
+// calling the function.
+func IsTemporary(err error) bool {
+	var t interface{ Temporary() bool }
+	return errors.As(err, &t) && t.Temporary()
+}
+
+// IsTemporaryConnection returns if the
+// provided error was a low level retryable connection error.
+// It also returns the type, in string form,
+// for optional logging and metrics use.
+func IsTemporaryConnection(err error) (retryType string, isRetryable bool) {
+	if err != nil {
+		if errors.Is(err, syscall.ECONNRESET) {
+			return "econnreset", true
+		}
+
+		if errors.Is(err, syscall.ECONNABORTED) {
+			return "econnaborted", true
+		}
+
+		if errors.Is(err, syscall.ENOTCONN) {
+			return "enotconn", true
+		}
+
+		if errors.Is(err, syscall.EWOULDBLOCK) {
+			return "ewouldblock", true
+		}
+
+		if errors.Is(err, syscall.EAGAIN) {
+			return "eagain", true
+		}
+
+		if errors.Is(err, syscall.ETIMEDOUT) {
+			return "etimedout", true
+		}
+
+		if errors.Is(err, syscall.EINTR) {
+			return "eintr", true
+		}
+
+		if errors.Is(err, syscall.EPIPE) {
+			return "epipe", true
+		}
+	}
+	return "", false
 }
