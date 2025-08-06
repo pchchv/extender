ab83e8ffc8f2ab9a4bf5ebc81a04a0934f6d2d37
pchchv <ipchchv@gmail.com>
2025-08-06 09:22:30 +0300

    Add OnRetryFn type

--- errors/do.go
+++ errors/do.go
@@ -3,6 +3,7 @@ package errorsext
 import (
 	"context"
 
+	optionext "github.com/pchchv/extender/values/option"
 	resultext "github.com/pchchv/extender/values/result"
 )
 
@@ -11,3 +12,8 @@ type IsRetryableFn[E any] func(err E) (reason string, isRetryable bool)
 
 // RetryableFn is a function that can be retried.
 type RetryableFn[T, E any] func(ctx context.Context) resultext.Result[T, E]
+
+// OnRetryFn is called after IsRetryableFn returns true and before the retry is attempted.
+//
+// this allows for interception, short-circuiting and adding of backoff strategies.
+type OnRetryFn[E any] func(ctx context.Context, originalErr E, reason string, attempt int) optionext.Option[E]
