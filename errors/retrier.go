ee226fd992d24767ebd1d672abe726089bd85160
pchchv <ipchchv@gmail.com>
2025-08-06 09:37:06 +0300

    Add BackoffFn type

--- errors/retrier.go
+++ errors/retrier.go
@@ -1,6 +1,18 @@
 package errorsext
 
+import "context"
+
 // MaxAttemptsMode is used to set the mode for the maximum number of attempts.
 //
-// E. g. Should the max attempts apply to all errors, just ones not determined to be retryable, reset on retryable errors, etc.
+// E. g. Should the max attempts apply to all errors,
+// just ones not determined to be retryable,
+// reset on retryable errors, etc.
 type MaxAttemptsMode uint8
+
+// BackoffFn is a function used to apply a backoff strategy to the retryable function.
+//
+// It accepts `E` in cases where the amount of time to backoff is dynamic,
+// for example when and http request fails with a 429 status code,
+// the `Retry-After` header can be used to determine how long to backoff.
+// It is not required to use or handle `E` and can be ignored if desired.
+type BackoffFn[E any] func(ctx context.Context, attempt int, e E)
