26520105c7b31b1c14ba9082d79ceb2e00246763
pchchv <ipchchv@gmail.com>
2025-08-06 09:21:31 +0300

    Add RetryableFn type

--- errors/do.go
+++ errors/do.go
@@ -1,4 +1,13 @@
 package errorsext
 
+import (
+	"context"
+
+	resultext "github.com/pchchv/extender/values/result"
+)
+
 // IsRetryableFn is called to determine if the error is retryable and optionally returns the reason for logging and metrics.
 type IsRetryableFn[E any] func(err E) (reason string, isRetryable bool)
+
+// RetryableFn is a function that can be retried.
+type RetryableFn[T, E any] func(ctx context.Context) resultext.Result[T, E]
