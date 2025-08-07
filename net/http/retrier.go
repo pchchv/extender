da67a65c4f7aa1330224b7fedfe2aad6dc0b2e4b
pchchv <ipchchv@gmail.com>
2025-08-07 19:48:10 +0300

    Implement Error and IsRetryable methods for ErrStatusCode

--- net/http/retrier.go
+++ net/http/retrier.go
@@ -1,6 +1,9 @@
 package httpext
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
 
 // ErrStatusCode can be used to treat/indicate a status code as an error and ability to indicate if it is retryable.
 type ErrStatusCode struct {
@@ -8,4 +11,14 @@ type ErrStatusCode struct {
 	IsRetryableStatusCode bool        // indicates if the status code is considered retryable
 	Headers               http.Header // contains the headers from the HTTP response
 	Body                  []byte      // the optional body of the HTTP response
+}
+
+// Error returns the error message for the status code.
+func (e ErrStatusCode) Error() string {
+	return "status code encountered: " + strconv.Itoa(e.StatusCode)
+}
+
+// IsRetryable returns if the provided status code is considered retryable.
+func (e ErrStatusCode) IsRetryable() bool {
+	return e.IsRetryableStatusCode
 }
