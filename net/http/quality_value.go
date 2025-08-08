413e9cc318ff1824f03b32c13f9cab2aab17f998
pchchv <ipchchv@gmail.com>
2025-08-08 22:42:28 +0300

    Implement a QualityValue function

---
+++ net/http/quality_value.go
@@ -0,0 +1,20 @@
+package httpext
+
+import "fmt"
+
+// QualityValueFormat is a format string helper for Quality Values
+const QualityValueFormat = "%s;q=%1.3g"
+
+// QualityValue accepts a values to add/concatenate
+// a quality values to and the quality values itself.
+func QualityValue(v string, qv float32) string {
+	if qv > 1 {
+		qv = 1 // highest possible values
+	}
+
+	if qv < 0.001 {
+		qv = 0.001 // lowest possible values
+	}
+
+	return fmt.Sprintf(QualityValueFormat, v, qv)
+}
