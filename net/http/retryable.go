1c883125a7fac7e2a48db74b7b7a0ec1d1cd88fc
pchchv <ipchchv@gmail.com>
2025-08-11 09:38:05 +0300

    Implement an IsNonRetryableStatusCode function

--- net/http/retryable.go
+++ net/http/retryable.go
@@ -14,9 +14,44 @@ var (
 		// origin server but did not receive response within 100 seconds and so times out.
 		524: true,
 	}
+	// nonRetryableStatusCodes defines common HTTP responses that are
+	// not considered never to be retryable.
+	nonRetryableStatusCodes = map[int]bool{
+		http.StatusBadRequest:                    true,
+		http.StatusUnauthorized:                  true,
+		http.StatusForbidden:                     true,
+		http.StatusNotFound:                      true,
+		http.StatusMethodNotAllowed:              true,
+		http.StatusNotAcceptable:                 true,
+		http.StatusProxyAuthRequired:             true,
+		http.StatusConflict:                      true,
+		http.StatusLengthRequired:                true,
+		http.StatusPreconditionFailed:            true,
+		http.StatusRequestEntityTooLarge:         true,
+		http.StatusRequestURITooLong:             true,
+		http.StatusUnsupportedMediaType:          true,
+		http.StatusRequestedRangeNotSatisfiable:  true,
+		http.StatusExpectationFailed:             true,
+		http.StatusTeapot:                        true,
+		http.StatusMisdirectedRequest:            true,
+		http.StatusUnprocessableEntity:           true,
+		http.StatusPreconditionRequired:          true,
+		http.StatusRequestHeaderFieldsTooLarge:   true,
+		http.StatusUnavailableForLegalReasons:    true,
+		http.StatusNotImplemented:                true,
+		http.StatusHTTPVersionNotSupported:       true,
+		http.StatusLoopDetected:                  true,
+		http.StatusNotExtended:                   true,
+		http.StatusNetworkAuthenticationRequired: true,
+	}
 )
 
 // IsRetryableStatusCode returns true if the provided status code is considered retryable.
 func IsRetryableStatusCode(code int) bool {
 	return retryableStatusCodes[code]
+}
+
+// IsNonRetryableStatusCode returns true if the provided status code should generally not be retryable.
+func IsNonRetryableStatusCode(code int) bool {
+	return nonRetryableStatusCodes[code]
 }
