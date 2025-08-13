d8cd0f3ec1d6f45f10ce09bcf302732b7f10d8b3
pchchv <ipchchv@gmail.com>
2025-08-13 09:05:44 +0300

    Impelemt Min function

---
+++ math/min.go
@@ -0,0 +1,12 @@
+package mathext
+
+import constraintsext "github.com/pchchv/extender/constraints"
+
+// Min returns the smaller value.
+//
+// NOTE: this function does not check for difference in floats of 0/zero vs -0/negative zero using Signbit.
+//
+// Deprecated: use the new std library `max` instead.
+func Min[N constraintsext.Number](x, y N) N {
+	return min(x, y)
+}
