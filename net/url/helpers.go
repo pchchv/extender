e43b76e8b40d7987ef78390c3853b5b386e15c6a
pchchv <ipchchv@gmail.com>
2025-08-13 08:57:18 +0300

    Implement EncodeToURLValues function

--- net/url/helpers.go
+++ net/url/helpers.go
@@ -1,1 +1,12 @@
 package urlext
+
+import (
+	"net/url"
+
+	httpext "github.com/pchchv/extender/net/http"
+)
+
+// EncodeToURLValues encodes a struct or field into a set of url.Values.
+func EncodeToURLValues(v interface{}) (url.Values, error) {
+	return httpext.DefaultFormEncoder.Encode(v)
+}
