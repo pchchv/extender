19c94d1d96d26bcbeb07cd05f43bbfc5561ffbb2
pchchv <ipchchv@gmail.com>
2025-08-14 10:23:23 +0300

    Implement constructor for detachedContext type

--- context/context.go
+++ context/context.go
@@ -5,3 +5,17 @@ import "context"
 type detachedContext struct {
 	parent context.Context
 }
+
+// Detach returns a new context which continues to have access to its parent values but
+// is no longer bound/attached to the parents timeouts nor deadlines.
+//
+// If a nil context is passed in a new Background context will be used as the parent.
+//
+// This is useful for when you wish to pass along values such as span or logging information but
+// do not want the current operation to be cancelled despite what upstream code/callers think.
+func Detach(parent context.Context) detachedContext {
+	if parent == nil {
+		return detachedContext{parent: context.Background()}
+	}
+	return detachedContext{parent: parent}
+}
