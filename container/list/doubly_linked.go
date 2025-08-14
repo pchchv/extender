1993198b91e4762fd5ea6fdc68e98ef11481695b
pchchv <ipchchv@gmail.com>
2025-08-14 10:36:01 +0300

    implement Next and Prev methods for Node

--- container/list/doubly_linked.go
+++ container/list/doubly_linked.go
@@ -6,3 +6,13 @@ type Node[V any] struct {
 	prev  *Node[V]
 	Value V
 }
+
+// Next returns the nodes next Value or nil if it is at the tail.
+func (n *Node[V]) Next() *Node[V] {
+	return n.next
+}
+
+// Prev returns the nodes previous Value or nil if it is at the head.
+func (n *Node[V]) Prev() *Node[V] {
+	return n.prev
+}
