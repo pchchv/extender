6692780620df94fc449914f07e234159adbeb0e6
pchchv <ipchchv@gmail.com>
2025-08-15 11:32:08 +0300

    Add unit tests for linked list

--- container/list/doubly_linked_test.go
+++ container/list/doubly_linked_test.go
@@ -170,6 +170,237 @@ func TestLinkedListInserts(t *testing.T) {
 	Equal(t, l.Back().Value, node2.Value)
 }
 
+func TestLinkedListPushFront(t *testing.T) {
+	l := NewDoublyLinked[int]()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// push some data and then re-check
+	zeroNode := l.PushFront(0)
+	oneNode := l.PushFront(1)
+	twoNode := l.PushFront(2)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 3)
+
+	// test next logic
+	Equal(t, zeroNode.Value, 0)
+	Equal(t, zeroNode.Next(), nil)
+	Equal(t, zeroNode.Prev().Value, 1)
+	Equal(t, zeroNode.Prev().Prev().Value, 2)
+	Equal(t, zeroNode.Prev().Prev().Prev(), nil)
+	Equal(t, oneNode.Value, 1)
+	Equal(t, oneNode.Next().Value, 0)
+	Equal(t, oneNode.Next().Next(), nil)
+	Equal(t, oneNode.Prev().Value, 2)
+	Equal(t, oneNode.Prev().Prev(), nil)
+	Equal(t, twoNode.Value, 2)
+	Equal(t, twoNode.Prev(), nil)
+	Equal(t, twoNode.Next().Value, 1)
+	Equal(t, twoNode.Next().Next().Value, 0)
+	Equal(t, twoNode.Next().Next().Next(), nil)
+
+	// remove middle node and test again
+	l.Remove(oneNode)
+	Equal(t, oneNode.Value, 1)
+	Equal(t, oneNode.Prev(), nil)
+	Equal(t, oneNode.Next(), nil)
+
+	// move to front
+	l.MoveToFront(zeroNode)
+	Equal(t, l.Front().Value, 0)
+	Equal(t, l.Back().Value, 2)
+
+	// move to back
+	l.MoveToBack(zeroNode)
+	Equal(t, l.Front().Value, 2)
+	Equal(t, l.Back().Value, 0)
+
+	// test clearing
+	l.Clear()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+}
+
+func TestLinkedListPushBack(t *testing.T) {
+	l := NewDoublyLinked[int]()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// push some data and then re-check
+	zeroNode := l.PushBack(0)
+	oneNode := l.PushBack(1)
+	twoNode := l.PushBack(2)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 3)
+
+	// test next logic
+	Equal(t, zeroNode.Value, 0)
+	Equal(t, zeroNode.Next().Value, 1)
+	Equal(t, zeroNode.Next().Next().Value, 2)
+	Equal(t, zeroNode.Next().Next().Next(), nil)
+	Equal(t, zeroNode.Prev(), nil)
+	Equal(t, oneNode.Value, 1)
+	Equal(t, oneNode.Next().Value, 2)
+	Equal(t, oneNode.Next().Next(), nil)
+	Equal(t, oneNode.Prev().Value, 0)
+	Equal(t, oneNode.Prev().Prev(), nil)
+	Equal(t, twoNode.Value, 2)
+	Equal(t, twoNode.Prev().Value, 1)
+	Equal(t, twoNode.Prev().Prev().Value, 0)
+	Equal(t, twoNode.Prev().Prev().Prev(), nil)
+	Equal(t, twoNode.Next(), nil)
+
+	// remove middle node and test again
+	l.Remove(oneNode)
+	Equal(t, oneNode.Value, 1)
+	Equal(t, oneNode.Prev(), nil)
+	Equal(t, oneNode.Next(), nil)
+
+	// move to front
+	l.MoveToBack(zeroNode)
+	Equal(t, l.Front().Value, 2)
+	Equal(t, l.Back().Value, 0)
+
+	// move to back
+	l.MoveToFront(zeroNode)
+	Equal(t, l.Front().Value, 0)
+	Equal(t, l.Back().Value, 2)
+
+	// test clearing
+	l.Clear()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+}
+
+func TestLinkedListMoving(t *testing.T) {
+	l := NewDoublyLinked[int]()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// test pushing after with one node
+	node1 := l.PushFront(0)
+	node2 := l.PushAfter(node1, 1)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 2)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Back().Value, node2.Value)
+
+	// test moving after with two nodes
+	l.MoveAfter(node2, node1)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 2)
+	Equal(t, l.Front().Value, node2.Value)
+	Equal(t, l.Back().Value, node1.Value)
+
+	// test clearing
+	l.Clear()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// test pushing before with one node
+	node1 = l.PushFront(0)
+	node2 = l.PushBefore(node1, 1)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 2)
+	Equal(t, l.Front().Value, node2.Value)
+	Equal(t, l.Back().Value, node1.Value)
+
+	// test moving before with two nodes
+	l.MoveBefore(node2, node1)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 2)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Back().Value, node2.Value)
+
+	// test clearing
+	l.Clear()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// testing the same as above BUT with 3 nodes attached
+	node1 = l.PushFront(0)
+	node2 = l.PushAfter(node1, 1)
+	node3 := l.PushAfter(node2, 2)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 3)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Front().Next().Value, node2.Value)
+	Equal(t, l.Back().Value, node3.Value)
+	Equal(t, l.Back().Prev().Value, node2.Value)
+
+	l.MoveBefore(node2, node3)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 3)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Front().Next().Value, node3.Value)
+	Equal(t, l.Back().Value, node2.Value)
+	Equal(t, l.Back().Prev().Value, node3.Value)
+
+	l.MoveAfter(node3, node1)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 3)
+	Equal(t, l.Front().Value, node3.Value)
+	Equal(t, l.Front().Next().Value, node1.Value)
+	Equal(t, l.Back().Value, node2.Value)
+	Equal(t, l.Back().Prev().Value, node1.Value)
+
+	// test clearing
+	l.Clear()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// testing the same as above BUT with 4 nodes attached moving the middle nodes back and forth
+	node1 = l.PushFront(0)
+	node2 = l.PushAfter(node1, 1)
+	node3 = l.PushAfter(node2, 2)
+	node4 := l.PushAfter(node3, 3)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 4)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Front().Next().Value, node2.Value)
+	Equal(t, l.Front().Next().Next().Value, node3.Value)
+	Equal(t, l.Front().Next().Next().Next().Value, node4.Value)
+	Equal(t, l.Front().Next().Next().Next().Next(), nil)
+	Equal(t, l.Back().Value, node4.Value)
+	Equal(t, l.Back().Prev().Value, node3.Value)
+	Equal(t, l.Back().Prev().Prev().Value, node2.Value)
+	Equal(t, l.Back().Prev().Prev().Prev().Value, node1.Value)
+	Equal(t, l.Back().Prev().Prev().Prev().Prev(), nil)
+
+	l.MoveAfter(node3, node2)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 4)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Front().Next().Value, node3.Value)
+	Equal(t, l.Front().Next().Next().Value, node2.Value)
+	Equal(t, l.Front().Next().Next().Next().Value, node4.Value)
+	Equal(t, l.Front().Next().Next().Next().Next(), nil)
+	Equal(t, l.Back().Value, node4.Value)
+	Equal(t, l.Back().Prev().Value, node2.Value)
+	Equal(t, l.Back().Prev().Prev().Value, node3.Value)
+	Equal(t, l.Back().Prev().Prev().Prev().Value, node1.Value)
+	Equal(t, l.Back().Prev().Prev().Prev().Prev(), nil)
+
+	l.MoveAfter(node2, node3)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 4)
+	Equal(t, l.Front().Value, node1.Value)
+	Equal(t, l.Front().Next().Value, node2.Value)
+	Equal(t, l.Front().Next().Next().Value, node3.Value)
+	Equal(t, l.Front().Next().Next().Next().Value, node4.Value)
+	Equal(t, l.Front().Next().Next().Next().Next(), nil)
+	Equal(t, l.Back().Value, node4.Value)
+	Equal(t, l.Back().Prev().Value, node3.Value)
+	Equal(t, l.Back().Prev().Prev().Value, node2.Value)
+	Equal(t, l.Back().Prev().Prev().Prev().Value, node1.Value)
+	Equal(t, l.Back().Prev().Prev().Prev().Prev(), nil)
+
+	// test clearing
+	l.Clear()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+}
+
 func BenchmarkDoublyLinkedList_STD(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		l := list.New()
