f97b35abbf50f5ad209057ef13d0aa06a36c9cad
pchchv <ipchchv@gmail.com>
2025-08-15 11:25:35 +0300

    Add unit tests

--- container/list/doubly_linked_test.go
+++ container/list/doubly_linked_test.go
@@ -57,6 +57,93 @@ func TestSingleEntryPopFront(t *testing.T) {
 	Equal(t, back, nil)
 }
 
+func TestDoubleEntryPopBack(t *testing.T) {
+	l := NewDoublyLinked[int]()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// push some data and then re-check
+	zeroNode := l.PushFront(0)
+	oneNode := l.PushFront(1)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 2)
+	Equal(t, zeroNode.Value, 0)
+	Equal(t, oneNode.Value, 1)
+	Equal(t, zeroNode.Prev().Value, 1)
+	Equal(t, zeroNode.Next(), nil)
+	Equal(t, oneNode.Prev(), nil)
+	Equal(t, oneNode.Next().Value, 0)
+
+	back := l.PopBack()
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 1)
+	Equal(t, back.Value, 0)
+	Equal(t, back.Next(), nil)
+	Equal(t, back.Prev(), nil)
+	Equal(t, l.Front().Value, 1)
+	Equal(t, l.Back().Value, 1)
+
+	back2 := l.PopBack()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+	Equal(t, back2.Value, 1)
+	Equal(t, back2.Next(), nil)
+	Equal(t, back2.Prev(), nil)
+	Equal(t, l.Front(), nil)
+	Equal(t, l.Back(), nil)
+}
+
+func TestTripleEntryPopBack(t *testing.T) {
+	l := NewDoublyLinked[int]()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// push some data and then re-check
+	zeroNode := l.PushFront(0)
+	oneNode := l.PushFront(1)
+	twoNode := l.PushFront(2)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 3)
+	Equal(t, zeroNode.Value, 0)
+	Equal(t, oneNode.Value, 1)
+	Equal(t, twoNode.Value, 2)
+	Equal(t, zeroNode.Next(), nil)
+	Equal(t, zeroNode.Prev().Value, 1)
+	Equal(t, zeroNode.Prev().Prev().Value, 2)
+	Equal(t, zeroNode.Prev().Prev().Prev(), nil)
+	Equal(t, oneNode.Next().Value, 0)
+	Equal(t, oneNode.Next().Next(), nil)
+	Equal(t, oneNode.Prev().Value, 2)
+	Equal(t, oneNode.Prev().Prev(), nil)
+	Equal(t, twoNode.Prev(), nil)
+	Equal(t, twoNode.Next().Value, 1)
+	Equal(t, twoNode.Next().Next().Value, 0)
+	Equal(t, twoNode.Next().Next().Next(), nil)
+
+	// remove front
+	l.Remove(twoNode)
+
+	// remove back
+	l.Remove(zeroNode)
+}
+
+func TestLinkedListRemoveSingleEntry(t *testing.T) {
+	l := NewDoublyLinked[int]()
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+
+	// test pushing after with one node
+	node := l.PushFront(0)
+	Equal(t, l.IsEmpty(), false)
+	Equal(t, l.Len(), 1)
+	Equal(t, l.Front().Value, node.Value)
+	Equal(t, l.Back().Value, node.Value)
+
+	l.Remove(node)
+	Equal(t, l.IsEmpty(), true)
+	Equal(t, l.Len(), 0)
+}
+
 func BenchmarkDoublyLinkedList_STD(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		l := list.New()
